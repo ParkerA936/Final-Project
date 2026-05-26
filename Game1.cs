00e85b5a95ebe973170b3afe964d72d76bcdca53
Parker Arnett <362581936@GoTVDSB.ca>
2026-05-26 13:16:27 -0400

    blah

--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,8 @@ namespace Final_Project
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D backTexture, blockTexture;
-        Rectangle window, blockRect, blockRect1;
+        Texture2D backTexture, blockTexture, circleTexture, lineTexture, ballTexture;
+        Rectangle window, blockRect, blockRect1, circleRect, lineRect, ballRect;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,17 +26,22 @@ namespace Final_Project
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
 
-            blockRect = new Rectangle(10,100,10,40);
-            blockRect1 = new Rectangle();
+            ballRect = new Rectangle(477,276,50,50);
+            lineRect = new Rectangle(498,0,10,600);
+            circleRect = new Rectangle(452,250,100,100);
+            blockRect = new Rectangle(10,225,20,150);
+            blockRect1 = new Rectangle(970, 225, 20, 150);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            ballTexture = Content.Load<Texture2D>("Ball (1)");
             backTexture = Content.Load<Texture2D>("Blue vs Red");
-            blockTexture = Content.Load<Texture2D>("Blocks");
-
+            blockTexture = Content.Load<Texture2D>("White box");
+            circleTexture = Content.Load<Texture2D>("Circle_ (1)");
+            lineTexture = Content.Load<Texture2D>("Line (1)");
             // TODO: use this.Content to load your game content here
         }
 
@@ -57,6 +62,10 @@ namespace Final_Project
 
             _spriteBatch.Draw(backTexture, window, Color.White);
             _spriteBatch.Draw(blockTexture, blockRect, Color.White);
+            _spriteBatch.Draw(blockTexture, blockRect1, Color.White);
+            _spriteBatch.Draw(circleTexture, circleRect, Color.White);
+            _spriteBatch.Draw(lineTexture, lineRect, Color.White);
+            _spriteBatch.Draw(ballTexture, ballRect, Color.White);
 
             _spriteBatch.End();
             base.Draw(gameTime);
